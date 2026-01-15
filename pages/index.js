14af1c9087153306c4dfdd646895fe23c1635520
Jolti1985 <a.joltea@me.com>
2026-01-15 13:23:34 +0100

    Remove export from Home function in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,1 @@
-export default function Home(){return <h1>Rechnungs-Check Web Demo</h1>}
+https://chatgpt.com/backend-api/estuary/content?id=file_0000000026ac720a9f460d85b263bd97&ts=491244&p=fs&cid=1&sig=5c84b695a5b29624bacf225989bc8aa919690fcc200c86b6a4ab16f962ba9d81&v=0
